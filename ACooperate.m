912b55164cc273a1acd55c160e4c624fc630ec4e
j-aPfister <32663451+j-aPfister@users.noreply.github.com>
2017-11-29 07:36:26 +0100

    Delete ACooperate.m

--- ACooperate.m
+++
@@ -1,9 +0,0 @@
-%SP1= Strategy Player 1; SP2= Strategy Player 2 
-%Trust versus Always Cooperate
-%Cooperate=1, Defect=0
-function [SP1, SP2]= ACooperate(z3,w1,SP1,SP2,n)
-if z3(n)>w1(n) 
-    SP1(n)=1;
-else SP1(n)=0;
-end
-SP2(n)=1;
